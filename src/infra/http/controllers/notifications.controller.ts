808698ea2a244d4c2688aca886de8cce3b9d205e
Renan.Correa <renan.rcp16@gmail.com>
2022-12-13 22:33:23 -0300

    connecting to infra

---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,23 @@
+import { SendNotification } from './../../../app/useCases/send-notification';
+import { CreateNotificationBody } from '../dtos/create-notifications-body';
+import { Controller } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common/decorators';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    const { notification } = await this.sendNotification.execute({
+      recipientId,
+      content,
+      category,
+    });
+
+    return {
+      notification,
+    };
+  }
+}
